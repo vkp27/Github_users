d5b78aabe1683d2cbdd2d3ca1f6bd0e68f70dbf4
vaibhav <vaibhavkrpandey111@gmail.com>
2022-03-27 01:40:33 +0530

    new commit

--- src/context/context.js
+++ src/context/context.js
@@ -5,3 +5,23 @@ import mockFollowers from './mockData.js/mockFollowers';
 import axios from 'axios';
 
 const rootUrl = 'https://api.github.com';
+
+//to create context
+const GithubContext = React.createContext();
+
+//Now we have access to two components
+// Provider and consumer
+//We can access by typing GithubContext.Provider
+
+const GithubProvider = ({children}) => {
+    const [githubUser, setGithubUser] = useState(mockUser);
+    const [repos, setRepos] = useState(mockRepos);
+    const [followers, setFollowers] = useState(mockFollowers);
+    return (
+        <GithubContext.Provider value={{githubUser, repos, followers}}>
+            {children}
+        </GithubContext.Provider>
+    );
+}
+
+export {GithubProvider, GithubContext};
